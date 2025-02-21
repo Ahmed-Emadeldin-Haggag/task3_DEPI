4fa5f3d3b25948ae59c2703e101d8ccdf40e0eb2
Ahmed Emadeldin Haggag <ahmedemadelden2005@gmail.com>
2025-02-21 11:41:42 +0200

    solving problems

--- app/src/main/java/com/example/task3_depi/IssueItem.kt
+++ app/src/main/java/com/example/task3_depi/IssueItem.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.githubreposappdepiround2.R
+import com.example.task3_DEPI.R
 import com.example.task3_depi.ui.theme.Task3_DEPITheme
 
 @Composable
